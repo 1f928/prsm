bf4bdf168f7a66379408fa74c6f2b33783f61ae7
oxl <ox@ox.ox>
2021-10-17 11:39:26 -0500

    info bar progress

--- src/comps/Info.js
+++ src/comps/Info.js
@@ -1,10 +1,19 @@
+import React, { useState } from 'react';
 
 import { FaInfoCircle } from 'react-icons/fa'
 
 export default function Info (props) {
+  const [visibility, setVisibility] = useState(false);
+  const hover = () => {
+    setVisibility(!visibility)
+  }
 
   return (
-    <div className="info">
+    <div className="info" onPointerEnter={hover} onPointerLeave={hover}>
+      <div className={`info-box ${visibility ? "" : "hidden"}`}>
+        <p>bh.dev v0.0.1</p>
+        <p>made w/ React</p>
+      </div>
       <FaInfoCircle />
     </div>
   );
