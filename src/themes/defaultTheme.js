3dee2e59927d866348a9e7a0c1d9f0829f754dd6
oxd <contact@bh.dev>
2021-10-20 11:09:19 -0500

    debug action

--- src/themes/defaultTheme.js
+++ src/themes/defaultTheme.js
@@ -8,10 +8,10 @@ const DefaultTheme = {
         "--fg-hover-color": "#ebc9ed"
     },
     dark: {
-        "--primary-bg-color": "#171532",
+        "--primary-bg-color": "#1d1b3e",
         "--primary-fg-color": "#ebc9ed",
         "--focus-fg-color": "#7c7cd9",
-        "--inside-border": "0px 0px 17px 5px #090922",
+        "--inside-border": "0px 0px 17px 5px #090922a8",
         "--fg-hover-color": "#e568a7"
     }
 };
