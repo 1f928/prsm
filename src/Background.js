0c856abee4f2fd31610fe1b5c7596177fec00a6f
oxd <contact@bh.dev>
2021-10-27 19:12:42 -0500

    manifest changes and bg scaling fix

--- src/Background.js
+++ src/Background.js
@@ -4,8 +4,8 @@ const randRange = (max) => {
   return Math.floor(Math.random() * max);
 };
 
-const drawStar = (context, width, height, color) => {
-  const starSize =  1;
+const drawStar = (context, width, height, color, starSizeMultiplier) => {
+  const starSize = 1 * starSizeMultiplier;
   const x = randRange(width);
   const y = randRange(height);
 
@@ -20,10 +20,13 @@ function Background({theme, width, height}) {
   const ref = useRef(null);
   
   useEffect(() => {
+    const pixelRatio = window.devicePixelRatio;
+    const canvasWidth = width * pixelRatio;
+    const canvasHeight = height * pixelRatio;
     const canvas = ref.current;
     const context = canvas.getContext('2d');
-    canvas.width = width;
-    canvas.height = height;
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
     const starCount = Math.floor((width * height) / 5000);
     const fgColor = (theme === "light") ? '--fg-hover-color' : "--focus-fg-color"
 
@@ -31,7 +34,7 @@ function Background({theme, width, height}) {
     const starColor = getComputedStyle(document.getElementById("root")).getPropertyValue(fgColor) + "99";
     context.fillStyle = bgColor;
     context.fillRect(0,0,canvas.width,canvas.height);
-    Array.from({length: starCount}).forEach(_ => drawStar(context, width, height, starColor));
+    Array.from({length: starCount}).forEach(_ => drawStar(context, canvasWidth, canvasHeight, starColor, pixelRatio));
   }, [ref, theme, width, height]);
 
   return (
